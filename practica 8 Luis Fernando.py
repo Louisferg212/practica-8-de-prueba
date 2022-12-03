a4eef1ac1c7abc9a56f0d6c557473a5d053aa75a
Louisferg212 <118074558+Louisferg212@users.noreply.github.com>
2022-12-03 10:58:25 -0600

    Add files via upload

--- practica 8 Luis Fernando.py
+++ practica 8 Luis Fernando.py
@@ -1,27 +1,50 @@
 import os
-lista=[]
-#subprograma
+
+lista = []
+import os
+
+lista = []
+
+
+# subprograma
 def crearArchivo():
-    compras=open("datosEmpleados.txt","w")
+    empleados = open("datosEmpleados.txt", "w")
     print("El archivo ha sido creado")
-    compras.close()
+    empleados.close()
+
+
 def ingresarempleados():
-    nombreEmpleado=input("Digite el nombre del producto que desee comprar:")
-    compras=open("DatosEmpleados.txt","a")
-    compras.write(nombreEmpleado"\n")
+    nombreEmpleado = input("Digite el nombre el nombre del empleado:")
+    lista.append(nombreEmpleado)
+    empleados = open("DatosEmpleados.txt", "a")
+    empleados.write(nombreEmpleado+ "\n")
     print("Los nombres de los empleados fueron guardado correctamente")
-    compras.close()
-
+    empleados.close()
 def consultarempleados():
-
+    g = input("Digite el nombre del empleado") in lista
+    print(g)
 
 def modificarempleados():
+        for x in range(len(lista)):
+            print(lista[x])
 
+        l = input("Digite el nombre del empleado que desea modificar")
+        lista.remove(l)
+        k = input("Digite el nuevo nombre del empleado que desea")
+        lista.append(k)
 def eliminarempleados():
+    for x in range(len(lista)):
+        print(lista(x))
 
 
+    w = input("Digite el nombre de la persona que desea eliminar de la lista")
+    lista.pop(w)
 def imprimirlosempleados():
-#Menu principal
+        empleados = open("datosEmpleados.txt", "r")
+        datos = empleados.read()
+        print(datos)
+        empleados.close()
+
 def main():
     while True:
         print("\t\t\t Menu Principal \t\t\t")
@@ -32,26 +55,27 @@ def main():
         print("5-imprimir los empleados del archivo")
         print("6-salir")
 
-        x=int(input("Digite el numero que desea ingresar"))
+        x = int(input("Digite el numero que desea ingresar"))
 
-        if x==1:
-            ingresarempleados():
+        if x == 1:
+            ingresarempleados()
 
-        elif x==2:
-            consultarempleados():
+        elif x == 2:
+            consultarempleados()
 
-        elif x==3:
-            modificarempleados():
+        elif x == 3:
+            modificarempleados()
 
-        elif x==4:
+        elif x == 4:
             eliminarempleados()
 
-        elif x==5:
+        elif x == 5:
             imprimirlosempleados()
 
-        elif x==6
+        elif x == 6:
             break
         else:
             print("Ningun dato es correcto intente nuevamente")
+
 
 main()
